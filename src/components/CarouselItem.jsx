5bcbba0e90933b600fd9f8274fd10d1f3afe148e
baiest <ballesteros.juan@correounivalle.edu.co>
2021-05-23 16:23:32 -0500

    Agregados datos de API fake

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,16 +3,16 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png'
 import plusIcon from '../assets/static/plus-icon.png'
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://images.pexels.com/photos/3171830/pexels-photo-3171830.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="imagen pexel.com" />
+        <img className="carousel-item__img" src={cover} alt={title} />
         <div className="carousel-item__details">
             <div className="carousel-item__options">
                 <img src={playIcon} alt="play" />
                 <img src={plusIcon} alt="plus" />
             </div>
-            <p className="carousel-item__details--title">Titulo descriptivo</p>
-            <p className="carousel-item__details--subtitle">2019 16+ 114min</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
         </div>
     </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../assets/styles/App.scss';
 import Header from '../components/Header'
 import Search from '../components/Search'
@@ -6,33 +6,58 @@ import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
-const App = () => (
+import useInitialState from '../hooks/useInitialState';
+
+const API = 'http://localhost:3000/initialState';
+
+const App = () => {
+    const initialState = useInitialState(API);
+    return (
     <div className='App'>
         <Header />
         <Search />
-        <Categories title="My List">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+        {
+            initialState.mylist?.length > 0 &&  
+            <Categories title="My List">
+                <Carousel>
+                {   initialState.mylist?
+                    initialState.mylist.map(item => (
+                    <CarouselItem key={item.id} {...item}/>
+                ))
+                :
+                <h4>Loading...</h4>
+            }
+                </Carousel>
+            </Categories>
+        }
         <Categories title="Tendencias">
             <Carousel>
-                <CarouselItem />
-                <CarouselItem />
+            {   initialState.trends?
+                initialState.trends.map(item => (
+                    <CarouselItem key={item.id} {...item}/>
+                ))
+                :
+                <h4>Loading...</h4>
+            }
             </Carousel>
         </Categories>
-        <Categories title="Originales de Platzi">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+        {
+            initialState.originals?.length > 0 &&  
+            <Categories title="Originales de platzi">
+                <Carousel>
+                {   initialState.originals?
+                    initialState.originals.map(item => (
+                    <CarouselItem key={item.id} {...item}/>
+                ))
+                :
+                <h4>Loading...</h4>
+            }
+                </Carousel>
+            </Categories>
+        }
         <Footer/>
     </div>
-);
+    );
+}
 
 export default App;
